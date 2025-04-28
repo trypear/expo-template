9339311bbc238224b7795fb5063e04ea0e754aa1
James Arnott <james@rno.tt>
2025-04-28 23:04:21 +0100

    fix: styling of the settings page

--- apps/mobile-app/app/(tabs)/settings.tsx
+++ apps/mobile-app/app/(tabs)/settings.tsx
@@ -1,4 +1,7 @@
-import { Button, StyleSheet } from "react-native";
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Button, Image, StyleSheet } from "react-native";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
@@ -10,21 +13,31 @@ export default function SettingsScreen() {
   const signOut = useSignOut();
 
   return (
-    <ThemedView style={styles.container}>
-      <ThemedView style={styles.section}>
-        <ThemedText type="title">Account</ThemedText>
-        <ThemedView style={styles.userInfo}>
-          <ThemedText type="subtitle">
-            {user && "name" in user ? user.name : "Not logged in"}
-          </ThemedText>
-        </ThemedView>
-        <Button
-          onPress={() => (user ? signOut() : signIn())}
-          title={user ? "Sign Out" : "Sign In With Discord"}
-          color="#5B65E9"
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+      headerImage={
+        <Image
+          source={require("@/assets/images/partial-react-logo.png")}
+          style={styles.reactLogo}
         />
+      }
+    >
+      <ThemedView style={styles.container}>
+        <ThemedView style={styles.section}>
+          <ThemedText type="title">Account</ThemedText>
+          <ThemedView style={styles.userInfo}>
+            <ThemedText type="subtitle">
+              {user && "name" in user ? user.name : "Not logged in"}
+            </ThemedText>
+          </ThemedView>
+          <Button
+            onPress={() => (user ? signOut() : signIn())}
+            title={user ? "Sign Out" : "Sign In With Discord"}
+            color="#5B65E9"
+          />
+        </ThemedView>
       </ThemedView>
-    </ThemedView>
+    </ParallaxScrollView>
   );
 }
 
@@ -41,5 +54,12 @@ const styles = StyleSheet.create({
   },
   userInfo: {
     paddingVertical: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
   },
 });

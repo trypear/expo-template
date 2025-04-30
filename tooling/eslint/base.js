d3559320fe611f07a3049bb5fcbab2e4c0cf2aad
James Arnott <james@rno.tt>
2025-04-30 11:48:24 +0100

    chore: relaxed eslint

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -78,7 +78,8 @@ export default tseslint.config(
       "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/no-unsafe-assignment": "off",
       "@typescript-eslint/no-unsafe-call": "off",
-      "@typescript-eslint/no-unsafe-argument": "off"
+      "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-unsafe-return": "off"
     },
   },
   {

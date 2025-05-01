1d2db0f95664746b1436347c865877048a96b6b4
James Arnott <james@rno.tt>
2025-05-01 18:06:51 +0100

    fix: exports

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,13 +2,8 @@ export * from "./schema";
 export * from "./relations";
 export * from "./zod-schemas";
 export * from "./helpers";
-import { and, asc, desc, gt, gte, inArray, isNotNull, isNull, lt, lte, ne, not, notInArray, or, sql } from "drizzle-orm";
+export * from "drizzle-orm";
 import { alias } from "drizzle-orm/pg-core";
 import { eqi } from "./helpers";
 
-export {
-	and, asc, desc, gt, gte, inArray, isNotNull, isNull, lt, lte, ne, not, notInArray, or, sql,
-	alias,
-	// Export eqi as eq to replace the standard eq with our prefix-aware version
-	eqi as eq
-};
+export { eqi, alias };

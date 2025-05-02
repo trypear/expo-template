de08aab9d05c1b375c2517fd459eb67e36504799
James Arnott <james@rno.tt>
2025-05-02 11:32:00 +0100

    feat: added timothy (trpc)

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,10 +1,10 @@
 import { authRouter } from "./router/auth";
-import { testRouter } from "./router/test";
+import { exampleRouter } from "./router/example";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
-  test: testRouter,
+  example: exampleRouter,
 });
 
 // export type definition of API
